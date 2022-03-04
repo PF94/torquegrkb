bc5e2c9ac5d2a92ce6cb4c9f0017168893d52149
Chaz "Gamerappa" Péloquin <chazpelo2@gmail.com>
2022-03-04 00:57:42 -0500

    actually implement urlEnc

--- Engine/source/console/consoleFunctions.cpp
+++ Engine/source/console/consoleFunctions.cpp
@@ -2797,9 +2797,32 @@ DefineEngineFunction( getMaxDynamicVerts, S32, (),,
    return MAX_DYNAMIC_VERTS / 2;
 }
 
-DefineEngineFunction(urlEnc, void, (const char* address), ,
-	"ok so this is placeholder shit")
+// https://stackoverflow.com/a/29962178
+DefineEngineFunction(urlEnc, String, (String str), ,
+	"Get the encoded bullshit thing for the URL thing."
+	"@return the encoded url")
 {
-	Con::errorf("not actually implemented");
-	return;
+	String new_str = "";
+	char c;
+	int ic;
+	const char* chars = str.c_str();
+	char bufHex[10];
+	int len = strlen(chars);
+
+	for (int i = 0; i<len; i++) {
+		c = chars[i];
+		ic = c;
+		// uncomment this if you want to encode spaces with +
+		/*if (c==' ') new_str += '+';
+		else */if (isalnum(c) || c == '-' || c == '_' || c == '.' || c == '~') new_str += c;
+		else {
+			sprintf(bufHex, "%X", c);
+			if (ic < 16)
+				new_str += "%0";
+			else
+				new_str += "%";
+			new_str += bufHex;
+		}
+	}
+	return new_str;
 }
